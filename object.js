cfc34c5a440cbd89c79edad9c1ce0b92c1d09b97
sanket-niwate <137911756+sanket-niwate@users.noreply.github.com>
2024-01-20 12:29:47 +0530

    Update object.js

--- object.js
+++ object.js
@@ -9,7 +9,12 @@ const sanket={
 
 console.log(sanket);
 
+
+//dot notation
 console.log(sanket.name);//particular value
+
+//bracket notation
+console.log(sanket['name']);
 
 //object are key values pair
 const key=Object.keys(sanket);
@@ -20,3 +25,30 @@ console.log(value);
 
 const entry=Object.entries(sanket);
 console.log(entry);
+
+
+
+
+var cap={
+    name:'sanket',
+    friend:['sanket','niwate'],
+
+    a:{
+        id:12,
+        city:{
+          name:'neha',
+        }
+    }
+
+}
+
+
+console.log(cap.friend[1]);//niwate
+
+console.log(cap.a.city.name);//neha
+ cap.movies='age of ultron'
+ console.log(cap);
+
+
+ delete cap.movies
+ console.log(cap);
