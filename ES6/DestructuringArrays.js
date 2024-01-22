b13be4d6f4f033cb716979de4b2631c380e77527
sanket-niwate <137911756+sanket-niwate@users.noreply.github.com>
2024-01-22 20:44:58 +0530

    Add files via upload

---
+++ ES6/DestructuringArrays.js
@@ -0,0 +1,19 @@
+//the destructing assignment syntax is a  javascript expression that makes it possible to unpack values
+//from arrays or properties from objects into distinct variables .
+
+let arr=['hii','i','am','sanket']
+
+let a=(arr[0])
+
+console.log(a);
+console.log(arr[1]);
+
+
+
+let [b,c,d,e]=arr;0
+  
+
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
